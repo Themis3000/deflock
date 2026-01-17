6d07e856d92562ea499fd050a7f419da122bcde6
Themi Megas <Tcm4760@gmail.com>
2026-01-17 16:05:27 -0600

    added test setup methods

--- serverless/alpr_cache/src/tests/__init__.py
+++ serverless/alpr_cache/src/tests/__init__.py
@@ -1,5 +1,14 @@
 import unittest
+import json
 
 
 class AlprCacheTests(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        with open("test_overpass_data.json") as f:
+            cls.overpass_test_data_raw = f.read()
+
+    def setUp(self):
+        # On every test setup, overpass_test_data is reset just in case it's contents where mutated.
+        self.overpass_test_data = json.loads(self.overpass_test_data_raw)
+
